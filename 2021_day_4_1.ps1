7a235d55eac4464ffe640d69fe48da752dd645fd
Bernard Moeskops <bmoeskops@qap360.nl>
2021-12-09 10:16:06 +0100

    Day 4 - Part 1

--- 2021_day_4_1.ps1
+++ 2021_day_4_1.ps1
@@ -1,54 +1,64 @@
 $content = Get-Content .\2021_day_4_input.txt
-$bingo = $content[0].Split(",")
-$content = $content | Where-Object {($_ -ne $content[0])}
-$croppedContent = $content | Where-Object {($_)}
-$boards = $croppedContent.count / 3
-$scoreObject = @()
-
-0..($boards - 1) | ForEach-Object {
-    $scoreObject += New-Object psobject -Property @{
-        $_ = New-Object psobject -Property @{
-            0 = 0
-            1 = 0
-            2 = 0
-            3 = 0
-            4 = 0
-            5 = 0
-            a = 0
-            b = 0
-            c = 0
-            d = 0
-            e = 0
-        }
-    }
-}
-
-foreach ($nr in $bingo) {
-    0..($boards - 1) | Foreach-Object {
-        $row = $croppedContent[$_]
-        $rowCount = $_
-        $rowArray = $row.Split(" ")
-        $rowArray = $rowArray | Where-Object {($_)}
+$numbers = $content[0].Split(",")
+$boards = $content | Where-Object {($_ -and $_ -ne $content[0])}
+$boardsAmount = $boards.count / 5
+$skipRow = 0
+$takeRow = 5
+$bingoWon = $false
+$skip = 0
+$take = 5
+do{
+    1..$boardsAmount | ForEach-Object {
+        $skipRow = ($_ * 5) - 5
+        $takeRow = ($_ * 5)
+        $board = $_
         0..4 | ForEach-Object {
-            $checkNr = $rowArray[$_]
-            if($checkNr -eq $nr){
-                $scoreObject[$rowCount][$_] = 1
-                #$croppedContent[$rowCount]["$_"] = "M$nr"
+            $index = $_
+            $verticalBingo = 0
+            $skipRow..($takeRow - 1) | ForEach-Object {
+                $row = $_
+                $nr = ($boards[$row].split(" ") | Where-Object {($_)})[$index]
+                if($nr -in $numbers[$skip..($take - 1)]){
+                    $verticalBingo++
+                }
+            }
+            if($verticalBingo -eq 5){
+                $numbersDrawn = $numbers[$skip..($take - 1)]
+                Write-Host "Vertical bingo at column $index which is board $board"
+                Write-Host "$($numbersDrawn.count) numbers drawn: $numbersDrawn"
+                $bingoWon = $true
+                break
             }
         }
+        $skipRow..($takeRow - 1) | ForEach-Object {
+            $row = $_
+            $horizontalBingo = 0
+            ($boards[$row].split(" ") | Where-Object {($_)}) | ForEach-Object {
+                if($_ -in $numbers[$skip..($take - 1)]){
+                    $horizontalBingo++
+                }
+            }
+            if($horizontalBingo -eq 5){
+                $numbersDrawn = $numbers[$skip..($take - 1)]
+                Write-Host "Horizontal bingo at row $_ which is board $board"
+                Write-Host "$($numbersDrawn.count) numbers drawn: $numbersDrawn"
+                $bingoWon = $true
+                break
+            }
+        }
+
     }
-}
+    $take++
+}while(!$bingoWon -and $numbersDrawn.count -le $numbers.count)
 
-
-$count = 0
-$content | Foreach-Object {
-    if(!$_){
-        $count = 0
-    }else{
-        $count++
+if($bingoWon){
+    $check = $boards[(($board * 5) - 5)..(($board * 5) - 1)]
+    $uncheckedNumbers = 0
+    $check = $check.split(" ") | Where-Object {($_)}
+    $check | ForEach-Object {
+        if($_ -notin $numbersDrawn){
+            $uncheckedNumbers += $_
+        }
     }
-}
-
-foreach($nr in $bingo){
-
+    Write-Host "Answer is: $($uncheckedNumbers * $numbersDrawn[-1])"
 }
