33fa54ff8ed6be671ef4e49b86471f97b80f0bac
Bernard Moeskops <bmoeskops@qap360.nl>
2021-12-15 00:25:18 +0100

    Day 9

---
+++ 2021_day_9_1.ps1
@@ -0,0 +1,48 @@
+﻿$content = Get-Content .\2021_day_9_input.txt
+$index = New-Object 'object[,]' ($content[0].Length + 1),($content.Count + 1)
+$count = 0
+foreach($row in $content){
+    for($i = 0;$i -lt ($row.Length);$i++){
+        $index[$i,$count] = $row[$i]
+    }
+    $count++
+}
+
+
+$lowNumbers = @()
+[int]$y = 0
+foreach($row in $content){
+    [int]$x = 0
+    foreach($number in ($row.ToCharArray())){
+        $surroundingNumbers = @()
+        if($index[$x,($y - 1)]){
+            $surroundingNumbers += $index[$x,($y - 1)] #up
+        }
+        if($index[($x - 1),$y]){
+            $surroundingNumbers += $index[($x - 1),$y] #left
+        }
+        if($index[($x + 1),$y]){
+            $surroundingNumbers += $index[($x + 1),$y] #right
+        }
+        if($index[$x,($y + 1)]){
+            $surroundingNumbers += $index[$x,($y + 1)] #down
+        }
+        $noLowNumber = $false
+        foreach($n in $surroundingNumbers){
+            if([int]$n -le $number){
+                $noLowNumber = $true
+            }
+        }
+        if(!$noLowNumber){
+            $lowNumbers += $number
+        }
+        $x++
+    }
+    $y++
+}
+
+[int]$total = 0
+foreach($num in $lowNumbers){
+    $total += [convert]::ToInt32($num, 10) + 1
+}
+$total
