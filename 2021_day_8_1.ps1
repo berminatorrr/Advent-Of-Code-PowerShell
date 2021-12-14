00b70987c79b9228ac3a1b074125b3d2ced835fc
Bernard Moeskops <bmoeskops@qap360.nl>
2021-12-14 23:17:20 +0100

    Day 8

---
+++ 2021_day_8_1.ps1
@@ -0,0 +1,22 @@
+﻿$content = Get-Content .\2021_day_8_input.txt
+$1 = 0
+$4 = 0
+$7 = 0
+$8 = 0
+
+$content | ForEach-Object {
+    $inValue = $_.split("|")[0]
+    $outValue = $_.split("|")[1]
+    $inValue = $inValue.Split(" ") | Where-Object {($_)}
+    $outValue = $outValue.Split(" ") | Where-Object {($_)}
+    $outValue | ForEach-Object {
+        switch($_.length){
+            2 {$1++}
+            3 {$7++}
+            4 {$4++}
+            7 {$8++}
+        }
+    }
+}
+
+Write-Host "Answer: $($1 + $4 + $7 + $8)"
